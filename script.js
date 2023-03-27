9eb286111e3c5772d080f39fc9a6bfe1ddd95999
Saquib Ansari <105703040+saquibansari09@users.noreply.github.com>
2023-03-26 22:16:27 -0700

    Add files via upload
    Create a single webPage using HTML,CSS and JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,6 @@
+const button2 = document.querySelector(".button");
+
+let smImgContainer = document.querySelector(".sm-product-img-container");
+let smImg = document.querySelector(".sm-product-img");
+let productIndex = document.querySelector(".product-index");
+let smProductPara = document.querySelector(".sm-product-para");
